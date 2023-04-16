10d55b80ca98a055450af4bb3d37a6f07de43f65
Tanin <landtanin@gmail.com>
2023-04-16 09:17:19 +0100

    add empty text error label

--- NotAnotherTodoApp/NewItemTableViewCell.swift
+++ NotAnotherTodoApp/NewItemTableViewCell.swift
@@ -13,6 +13,7 @@ class NewItemTableViewCell: UITableViewCell {
     
     var textField = UITextField()
     var doneButton = UIButton(type: .system)
+    private var errorLabel = UILabel()
     
     // you can use delegate here
     var onDoneButtonTapped: ((String) -> Void)?
@@ -23,23 +24,32 @@ class NewItemTableViewCell: UITableViewCell {
         // Add subviews to cell's content view
         contentView.addSubview(textField)
         contentView.addSubview(doneButton)
+        contentView.addSubview(errorLabel)
         
         // Configure text field
         textField.placeholder = "Enter new item"
         textField.borderStyle = .none
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.delegate = self
         
         // Configure done button
         doneButton.setTitle("Done", for: .normal)
         doneButton.translatesAutoresizingMaskIntoConstraints = false
         doneButton.addTarget(self, action: #selector(doneButtonTapped), for: .touchUpInside)
         
+        // Text field error label
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.textColor = .red
+        errorLabel.isHidden = true
+        
         // Add constraints
         NSLayoutConstraint.activate([
             textField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             textField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             doneButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            doneButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+            doneButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            errorLabel.leadingAnchor.constraint(equalTo: textField.leadingAnchor),
+            errorLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 12)
         ])
     }
     
@@ -49,9 +59,22 @@ class NewItemTableViewCell: UITableViewCell {
     
     @objc private func doneButtonTapped() {
         if let text = textField.text, !text.isEmpty {
+            errorLabel.isHidden = true
             onDoneButtonTapped?(text)
             textField.text = nil
+        } else {
+            errorLabel.text = "Please enter some text"
+            errorLabel.isHidden = false
         }
     }
     
+}
+
+extension NewItemTableViewCell: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let tf = textField.text, !tf.isEmpty {
+            errorLabel.isHidden = true
+        }
+        return true
+    }
 }
